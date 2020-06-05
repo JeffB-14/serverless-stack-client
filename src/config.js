1a8acc4677d490814b59f79661dd9f34b530cbfe
Jeff Beier <jeff.beier@cygrp.com>
2020-06-05 11:58:04 -0500

    setting up the React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "amos-smarttrackdna"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "http://26tny1tlyc.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_LYylUqRNV",
+      APP_CLIENT_ID: "5tvp0pnhdelpvjj0i5up1b7bpi",
+      IDENTITY_POOL_ID: "us-east-2:34e05b9e-9cc5-4949-bff8-466454cce36f"
+    }
+  };
