d1f10093296eb2e4295b87f3e3dddbc266c416ce
Jeff Beier <jeff.beier@cygrp.com>
2020-06-05 17:39:24 -0500

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "us-east-2",
-      BUCKET: "amos-smarttrackdna"
-    },
-    apiGateway: {
-      REGION: "us-east-2",
-      URL: "http://26tny1tlyc.execute-api.us-east-2.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_LYylUqRNV",
-      APP_CLIENT_ID: "5tvp0pnhdelpvjj0i5up1b7bpi",
-      IDENTITY_POOL_ID: "us-east-2:34e05b9e-9cc5-4949-bff8-466454cce36f"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51Gqn6WLMeIth7eUae8uEPu4od8XjRIsqMQdmG3fhLtnipj7pVrw0cmIXvrZ5rKq90yWfusec04LShvf3WkUSmvlP00ddVq4Ajy",
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "amos-smarttrackdna"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://26tny1tlyc.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_LYylUqRNV",
+    APP_CLIENT_ID: "5tvp0pnhdelpvjj0i5up1b7bpi",
+    IDENTITY_POOL_ID: "us-east-2:34e05b9e-9cc5-4949-bff8-466454cce36f"
+  }
+};
